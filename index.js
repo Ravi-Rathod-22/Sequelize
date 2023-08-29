1d086744f87d477341d8fd74da350a5b6f7e68e1
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-08-29 13:25:48 +0530

    Model Defination

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-import "./models/model.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+/*
+  When you use module import you have to specific file name 
+  at the end.
+*/
+import User from "./models/user.js";
+import sequelize from "./models/model.js";
 
 const app = express();
 
@@ -10,6 +19,18 @@ app.use(bodyParser.json());
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
+
+// Always First Drop the table if exists and then create a new table
+User.sync({ force: true });
+
+// Create a new table once
+// User.sync();
+
+// Delete hole table
+// User.drop();
+
+// Delete all tables
+// sequelize.drop();
 
 app.listen(3000, () => {
   console.log("App will run on: http://localhost:3000");
--- models/model.js
+++ models/model.js
@@ -1,10 +1,18 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("employeedb", "root", "Chits@123.", {
-  host: "localhost",
-  dialect:
-    "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
-});
+const sequelize = new Sequelize(
+  // process.env.DATABASE,
+  // process.env.USER_NAME,
+  // process.env.USER_PASSWORD,
+  "employeedb",
+  "root",
+  "Chits@123.",
+  {
+    host: "localhost",
+    dialect:
+      "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  }
+);
 
 try {
   sequelize.authenticate();
@@ -12,3 +20,5 @@ try {
 } catch (error) {
   console.error("Unable to connect to the database:", error);
 }
+
+export default sequelize;
